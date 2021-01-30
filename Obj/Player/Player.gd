a48517ebe1dd4e21c343bb4e0c26c94ed64e9ee5
santyarellano <santy.arellano039@gmail.com>
2021-01-30 09:00:28 -0600

    player movement & jump

--- Obj/Player/Player.gd
+++ Obj/Player/Player.gd
@@ -3,12 +3,13 @@ extends KinematicBody2D
 
 # Declare member variables here. Examples:
 export var walk_spd = 100
-export var jump_power = 1
-export var gravity = 30
-var walk_vel = 0
+export var jump_power = -300
+export var gravity = 1200
 var right = 0
 var left = 0
+var walk_vel = 0
 var velocity = Vector2()
+var jumping = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -34,11 +35,16 @@ func get_input():
 	else:
 		left = 0
 		
+	var jump_key = Input.is_action_just_pressed("ui_select")
+	if jump_key and is_on_floor():
+		jumping = true
+		velocity.y = jump_power
+		
 	velocity.x = walk_spd*(right - left)
 	
 func _physics_process(delta):
 	get_input()
 	velocity.y += gravity * delta
-	#if jumping and is_on_floor():
-	#	jumping = false
-	velocity = move_and_slide(velocity)
+	if jumping and is_on_floor():
+		jumping = false
+	velocity = move_and_slide(velocity, Vector2(0, -1))
