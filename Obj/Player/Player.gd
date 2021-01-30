2f3a1bf012ebc2039821776829db929c542d1a1e
NicolasBecerraMachado <76926388+NicolasBecerraMachado@users.noreply.github.com>
2021-01-29 20:35:53 -0600

    upd

--- Obj/Player/Player.gd
+++ Obj/Player/Player.gd
@@ -4,7 +4,7 @@ extends KinematicBody2D
 # Declare member variables here. Examples:
 export var walk_spd = 100
 export var jump_power = 1
-export var gravity = 0
+export var gravity = 30
 var walk_vel = 0
 var right = 0
 var left = 0
@@ -13,8 +13,9 @@ var velocity = Vector2()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position.x = 400
-	position.y = 300
+	#position.x = 400
+	#position.y = 300
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
