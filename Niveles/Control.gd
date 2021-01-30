6262ad1ec99b1056fcfda0697783f5f1860d5a1b
Karla Sánchez <A01730545@itesm.mx>
2021-01-30 10:38:45 -0600

    script botones

--- Niveles/Control.gd
+++ Niveles/Control.gd
@@ -14,3 +14,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Jugar_pressed():
+	get_tree().change_scene("res://Niveles/Playground.tscn")
+
+
+func _on_Salir_pressed():
+	get_tree().quit()
--- Obj/Player/Player.gd
+++ Obj/Player/Player.gd
@@ -1,50 +1,16 @@
-extends KinematicBody2D
+extends RigidBody2D
 
 
 # Declare member variables here. Examples:
-export var walk_spd = 100
-export var jump_power = -300
-export var gravity = 1200
-var right = 0
-var left = 0
-var walk_vel = 0
-var velocity = Vector2()
-var jumping = false
+# var a = 2
+# var b = "text"
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#position.x = 400
-	#position.y = 300
-	pass
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
-func get_input():
-	# Walking
-	if Input.is_action_pressed("ui_right"):
-		right = 1
-	else:
-		right = 0
-		
-	if Input.is_action_pressed("ui_left"):
-		left = 1
-	else:
-		left = 0
-		
-	var jump_key = Input.is_action_just_pressed("ui_select")
-	if jump_key and is_on_floor():
-		jumping = true
-		velocity.y = jump_power
-		
-	velocity.x = walk_spd*(right - left)
-	
-func _physics_process(delta):
-	get_input()
-	velocity.y += gravity * delta
-	if jumping and is_on_floor():
-		jumping = false
-	velocity = move_and_slide(velocity, Vector2(0, -1))
+#func _process(delta):
+#	pass
