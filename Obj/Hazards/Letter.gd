bde1a0173cb43f3dc2742e5f00b1bafad2b6a9e4
EricBL3 <ericbuitron3@gmail.com>
2021-01-30 19:11:26 -0600

    hacer que cartas desaparezcan al chocar

--- Obj/Hazards/Letter.gd
+++ Obj/Hazards/Letter.gd
@@ -2,11 +2,20 @@ extends RigidBody2D
 
 export var speed = 200
 
-
+func _ready():
+	set_contact_monitor(true)
+	set_max_contacts_reported(2)
 
 
 
 
 
 func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+
+
+
+
+func _on_Letter_body_shape_entered(body_id, body, body_shape, local_shape):
 	queue_free()
